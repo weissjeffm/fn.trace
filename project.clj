28eedf517d88e60a7822bd9a63ee822420b9e708
Jeff Weiss <jweiss@redhat.com>
2011-08-24 10:38:22 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject fn.trace "1.0.0-SNAPSHOT"
+  :description "A clojure trace library similar to clojure.contrib.trace but adds some flexibility."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [robert/hooke "1.1.1"]])
