6835543d38b51b32605a4415b10ac7d8ecef57db
Jeff Weiss <jweiss@redhat.com>
2011-09-06 16:41:49 -0400

    first attempt at html

--- src/fn/trace.clj
+++ src/fn/trace.clj
@@ -1,47 +1,53 @@
 (ns fn.trace
-  (:require [robert.hooke :as hook])
+  (:require [robert.hooke :as hook]
+            [clojure.contrib.prxml :as xml]
+            [clojure.zip :as zip])
   (:use [clojure.contrib.core :only [-?>]]))
 
 (def
  ^{:doc "Current stack depth of traced function calls."}
  *trace-depth* 0)
 
-(defn tracer
-  "This function is called by trace.  Prints to standard output, but
-  may be rebound to do anything you like.  'name' is optional."
-  [name value]
-  (println (str (when name (format "%6s: " name))  value)))
-
-(defn per-thread-tracer []
-  (let [tracefile-name (str (.getName (Thread/currentThread)) ".trace")]
-                                (fn [name value]
-                                  (let [s (str (when name (format "%6s: " name))  value "\n")]
-                                    (spit tracefile-name s
-                                          :append true)))))
-(defn trace
-  "Sends name (optional) and value to the tracer function, then
-  returns value.  May be wrapped around any expression without
-  affecting the result."
-  ([value] (trace nil value))
-  ([name value]
-     (tracer name (pr-str value))
-     value))
-
 (defn trace-indent
   "Returns an indentation string based on *trace-depth*"
   []
   (apply str (take *trace-depth* (repeat "|    "))))
+
+(defn clj-format [name value & [out?]]
+  (pr-str [*trace-depth* name value out?]))
+
+(defn text-format [name value & [out?]]
+  (let [label (when name (format "%6s: " name))]
+    (if out?
+      (str label (trace-indent) "=> " (pr-str value))
+      (str label (trace-indent) value))))
+
+(defn tracer
+  "This function is called by trace.  Prints to standard output, but
+  may be rebound to do anything you like.  'name' is optional."
+  [name value & [out?]]
+  (println (text-format name value)))
+
+(defn per-thread-tracer [& [formatter]]
+  (let [formatter (or formatter text-format)
+        tracefile-name (str (.getName (Thread/currentThread)) ".trace")
+        tr-file-writer (java.io.FileWriter. tracefile-name true)]
+    (fn [name value & [out?]]
+      (binding [*out* tr-file-writer]
+        (println (formatter name value out?))))))
+
+
 
 (defn trace-fn-call
   "Traces a single call to a function f with args.  'name' is the
   symbol name of the function."
   [name f args]
   (let [id (gensym "t")]
-    (tracer id (str (trace-indent) (pr-str (cons name args))))
+    (tracer id (cons name args))
     (let [[value err] (binding [*trace-depth* (inc *trace-depth*)]
                         (try [(apply f args) nil]
                              (catch Exception e [e e])))]
-      (tracer id (str (trace-indent) "=> " (pr-str value)))
+      (tracer id value true)
       (when err (throw err))
       value)))
 
@@ -113,3 +119,43 @@
                  (concat (mapcat all-fn-in-ns namespaces) fns))) ~@forms))
 
 
+(defn to-xml-tree "takes a filename that was created with clj-format, reads it and turns it into data that can be passed to prxml for html output."
+  [f]
+  (let [rdr (java.io.BufferedReader. 
+             (java.io.FileReader. f))
+        cdata (fn [form] [:script {:type "syntaxhighlighter" :class "brush: clj"}
+                         [:cdata! (str form)]])
+        data (loop [lines (line-seq rdr) acc [] ins-point [0]]
+               (clojure.pprint/pprint [ acc ins-point])
+               (if (empty? lines) acc
+                   (let [[d _ form out?] (-> lines first read-string)
+                         v (if out? (cdata form)
+                               [:div {:class "trace"} 
+                                     (cdata form)])]
+                     (recur (drop 1 lines)
+                            (assoc-in acc ins-point v)
+                            (if out?
+                              (let [popped (pop ins-point)]
+                                (conj (vec (butlast popped)) (inc (last popped))))
+                              (conj ins-point 3))))))]
+    (.close rdr)
+    data))
+
+(defn to-html "Takes a trace file produced with clj-format, turns it into html with syntax highlighting and css."
+  [f & [sh-url stylesheet-url]]
+
+  (xml/prxml [:doctype! "html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
+              "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\""]
+             [:html {:xmlns "http://www.w3.org/1999/xhtml"}
+              [:head
+               [:meta  {:http-equiv "Content-Type"
+                        :content "text/html;charset=utf-8"}
+                [:script {:type "text/javascript" :src (str sh-url "scripts/shCore.js")}]
+                [:script {:type "text/javascript" :src (str sh-url "scripts/shBrushClojure.js")}]
+                [:link {:type "text/css" :rel "sylesheet" :href stylesheet-url}]
+                [:script {:type "text/javascript"}
+                 "SyntaxHighlighter.defaults['gutter'] = false;
+                  SyntaxHighlighter.defaults['toolbar'] = false;
+                  SyntaxHighlighter.all();"]]]
+              [:body [:h1 "Trace log"]
+               (to-xml-tree f)]]))
