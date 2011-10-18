7887f22e46d7c73320dc150a4c2ab65ce05b5af7
Jeff Weiss <jweiss@redhat.com>
2011-10-18 15:55:37 -0400

    reverting unwise changes

--- src/fn/trace.clj
+++ src/fn/trace.clj
@@ -1,59 +1,47 @@
 (ns fn.trace
-  (:require [robert.hooke :as hook]
-            [clojure.contrib.prxml :as xml]
-            [clojure.java.io :as io]
-            [clojure.pprint :as pp])
+  (:require [robert.hooke :as hook])
   (:use [clojure.contrib.core :only [-?>]]))
 
 (def
  ^{:doc "Current stack depth of traced function calls."}
  *trace-depth* 0)
 
+(defn tracer
+  "This function is called by trace.  Prints to standard output, but
+  may be rebound to do anything you like.  'name' is optional."
+  [name value]
+  (println (str (when name (format "%6s: " name))  value)))
+
+(defn per-thread-tracer []
+  (let [tracefile-name (str (.getName (Thread/currentThread)) ".trace")]
+                                (fn [name value]
+                                  (let [s (str (when name (format "%6s: " name))  value "\n")]
+                                    (spit tracefile-name s
+                                          :append true)))))
+(defn trace
+  "Sends name (optional) and value to the tracer function, then
+  returns value.  May be wrapped around any expression without
+  affecting the result."
+  ([value] (trace nil value))
+  ([name value]
+     (tracer name (pr-str value))
+     value))
+
 (defn trace-indent
   "Returns an indentation string based on *trace-depth*"
   []
   (apply str (take *trace-depth* (repeat "|    "))))
-
-(defn clj-format [name value & [out?]]
-  (let [[printer miser margin] (if out?
-                                 [pp/simple-dispatch 120 150]
-                                 [pp/code-dispatch 90 110])
-        val (binding [pp/*print-miser-width* miser
-                      pp/*print-right-margin* margin]
-              (with-out-str (pp/with-pprint-dispatch printer
-                              (pp/pprint value))))]
-    (pr-str [*trace-depth* name val out?])))
-
-(defn text-format [name value & [out?]]
-  (let [label (when name (format "%6s: " name))]
-    (if out?
-      (str label (trace-indent) "=> " (pr-str value))
-      (str label (trace-indent) (pr-str value)))))
-
-(defn tracer
-  "This function is called by trace.  Prints to standard output, but
-  may be rebound to do anything you like.  'name' is optional."
-  [name value & [out?]]
-  (println (text-format name value)))
-
-(defn per-thread-tracer [& [formatter]]
-  (let [formatter (or formatter text-format)
-        tracefile-name (str (.getName (Thread/currentThread)) ".trace")
-        tr-file-writer (java.io.FileWriter. tracefile-name)]
-    (fn [name value & [out?]]
-      (binding [*out* tr-file-writer]
-        (println (formatter name value out?))))))
 
 (defn trace-fn-call
   "Traces a single call to a function f with args.  'name' is the
   symbol name of the function."
   [name f args]
   (let [id (gensym "t")]
-    (tracer id (cons name args))
+    (tracer id (str (trace-indent) (pr-str (cons name args))))
     (let [[value err] (binding [*trace-depth* (inc *trace-depth*)]
                         (try [(apply f args) nil]
-                             (catch Throwable e [e e])))]
-      (tracer id value true)
+                             (catch Exception e [e e])))]
+      (tracer id (str (trace-indent) "=> " (pr-str value)))
       (when err (throw err))
       value)))
 
@@ -93,7 +81,7 @@
                                    args))))
     (let [[value err] (binding [*trace-depth* (inc *trace-depth*)]
                         (try [(apply f args) nil]
-                             (catch Throwable e [e e])))]
+                             (catch Exception e [e e])))]
       (tracer id (str (trace-indent) "=> " (pr-str value)))
       (when err (throw err))
       value)))
@@ -125,57 +113,3 @@
                  (concat (mapcat all-fn-in-ns namespaces) fns))) ~@forms))
 
 
-(defn to-xml-tree "takes a filename that was created with clj-format, reads it and turns it into data that can be passed to prxml for html output."
-  [f]
-  (let [rdr (java.io.BufferedReader. 
-             (java.io.FileReader. f))
-        cdata (fn [form] [:script {:type "syntaxhighlighter" :class "brush: clj"}
-                         [:cdata! (str form)]])
-        data (loop [lines (line-seq rdr) acc [] ins-point [0]]
-               (comment (clojure.pprint/pprint [ acc ins-point]))
-               (if (empty? lines) acc
-                   (let [[d _ form out?] (-> lines first read-string)
-                         v (if out? (cdata form)
-                               [:div {:class "trace"} 
-                                     (cdata form)])]
-                     (recur (drop 1 lines)
-                            (assoc-in acc ins-point v)
-                            (if out?
-                              (let [popped (pop ins-point)]
-                                (conj (vec (butlast popped)) (inc (last popped))))
-                              (conj ins-point 3))))))]
-    (.close rdr)
-    data))
-
-(defn to-html "Takes a trace file produced with clj-format, turns it into html with syntax highlighting and css."
-  [f dest-dir & [sh-url]]
-
-  (binding [*out* (java.io.FileWriter. (str dest-dir "/" f ".html"))
-            xml/*prxml-indent* 4
-            xml/*html-compatible* true]
-    (xml/prxml [:doctype! "html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
-                "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\""]
-               [:html {:xmlns "http://www.w3.org/1999/xhtml"}
-                [:head
-                 [:meta  {:http-equiv "Content-Type"
-                          :content "text/html;charset=utf-8"}]
-                 [:title f]
-                 [:script {:type "text/javascript" :src (str sh-url "scripts/shCore.js")} " "]
-                 [:script {:type "text/javascript" :src (str sh-url "scripts/shBrushClojure.js")} " "]
-                 [:link {:type "text/css" :rel "stylesheet" :href "styles/shTrace.css"}]
-                 [:script {:type "text/javascript"}
-                  [:raw! "SyntaxHighlighter.defaults['gutter'] = false;
-                          SyntaxHighlighter.defaults['toolbar'] = false;
-                          SyntaxHighlighter.all();"]]]
-                [:body [:h1 "Trace log"]
-                 (vec (concat [:div {:class "highlight"}] (to-xml-tree f)))]])
-    (.close *out*)))
-
-(defn htmlify [dest-dir trace-files sh-url]
-  (let [style "styles/shTrace.css"
-        sfile (java.io.File. (str dest-dir "/" style))]
-    (.mkdirs (.getParentFile sfile))
-    (io/copy (-> (ClassLoader/getSystemClassLoader) (.getResourceAsStream style))
-              sfile))
-  (doseq [file trace-files]
-    (to-html file dest-dir sh-url)))
