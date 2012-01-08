05d4f8a13440e12de16252e4bae7bb776b9175ed
Jeff Weiss <jweiss@redhat.com>
2012-01-08 11:45:40 -0200

    inc version (apparently i have unpushed changes on other repo, will discard those later)

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject fn.trace "1.3.0.0"
+(defproject fn.trace "1.3.0.2"
   :description "A clojure trace library similar to clojure.contrib.trace but adds some flexibility."
   :dependencies [[org.clojure/clojure "1.3.0"]])
+b
