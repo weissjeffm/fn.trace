d52cb61c79424585f85eda363b41dddfda95be3a
Jeff Weiss <jweiss@redhat.com>
2012-01-08 11:46:11 -0200

    oops

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject fn.trace "1.3.0.2"
   :description "A clojure trace library similar to clojure.contrib.trace but adds some flexibility."
   :dependencies [[org.clojure/clojure "1.3.0"]])
-b
